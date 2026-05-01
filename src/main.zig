c1ecaf2e8f48d4e9d48c2807859383605eada1ef
HTRMC <143692436+HTRMC@users.noreply.github.com>
2026-05-01 22:28:10 +0200

    feat: log scanline progress to stderr

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,8 @@ pub fn main(init: std.process.Init) !void {
     const io = init.io;
     var stdout_writer = std.Io.File.stdout().writer(io, &.{});
     const stdout = &stdout_writer.interface;
+    var stderr_writer = std.Io.File.stderr().writer(io, &.{});
+    const stderr = &stderr_writer.interface;
 
     const image_width: u16 = 256;
     const image_height: u16 = 256;
@@ -11,6 +13,7 @@ pub fn main(init: std.process.Init) !void {
     try stdout.print("P3\n{} {}\n255\n", .{ image_width, image_height });
 
     for (0..image_height) |j| {
+        try stderr.print("\rScanlines remaining: {}", .{image_height - j});
         for (0..image_width) |i| {
             const r = @as(f64, @floatFromInt(i)) / @as(f64, @floatFromInt(image_width - 1));
             const g = @as(f64, @floatFromInt(j)) / @as(f64, @floatFromInt(image_height - 1));
@@ -23,4 +26,6 @@ pub fn main(init: std.process.Init) !void {
             try stdout.print("{} {} {}\n", .{ ir, ig, ib });
         }
     }
+
+    try stderr.print("\rDone.                 \n", .{});
 }
